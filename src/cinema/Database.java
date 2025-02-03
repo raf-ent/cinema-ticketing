7b8211f32cb59aa786105970a5333c777b84badd
raf-ent <rufson24@gmail.com>
2025-02-03 09:20:38 -0500

    sqlite dep

--- src/cinema/Database.java
+++ src/cinema/Database.java
@@ -9,13 +9,14 @@ public class Database {
 
     private String user = "user";
     private String password = "#1#2#3%1%2%3";
-    private String url = "jdbc:mysql://localhost/movieticketbookingsystem";
+    private String url = "jdbc:sqlite:movies.db";
     private Statement statement;
 
     public Database() {
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
-            statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+            statement = connection.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
                     ResultSet.CONCUR_READ_ONLY);
         } catch (Exception e) {
             e.printStackTrace();
